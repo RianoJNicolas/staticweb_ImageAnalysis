a35d39511c46f8a91d5ca9727d54e84ce3547c0e
rianojnicolas <rianojnicolas@gmail.com>
2023-11-29 11:23:34 -0500

    Agregando gui basica v1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+// componente de titulo
+function Title(props) {
+  return <h1>{props.title}</h1>;
+}
+
+// componente de cuadro de texto para insertar una URL
+function UrlInput(props) {
+  return <input type="text" onChange={props.onChange} />;
+}
+
+// componente de boton con texto "Analyze"
+function AnalyzeButton(props) {
+  return <button onClick={props.onClick}>Analyze</button>;
+}
+
+// componente de boton con texto "Generate"
+function GenerateButton(props) {
+  return <button onClick={props.onClick}>Generate</button>;
 }
 
 export default App;
